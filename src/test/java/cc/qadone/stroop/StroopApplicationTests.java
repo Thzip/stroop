db52dd9a350bab43e2b127901776da89bb29260a
yanghuawei <yanghuawei@hujiang.com>
2018-10-17 20:44:45 +0800

    init

---
+++ src/test/java/cc/qadone/stroop/StroopApplicationTests.java
@@ -0,0 +1,16 @@
+package cc.qadone.stroop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StroopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
